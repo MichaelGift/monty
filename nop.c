49060784e03b8721842f297682b6d9be331ad42a
Michael Gift <simiyugift@outlook.com>
2023-10-19 15:47:53 +0000

    Hear so evil do no evil

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * do_nothing- See no evil, do no evil
+ * @head: Pointer to stack head
+ * @counter: Line number
+ * Return: no return
+ */
+void do_nothing(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
+
