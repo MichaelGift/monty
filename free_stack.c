b6634420e92a45d0c64ed66346a01f024289de03
Michael Gift <simiyugift@outlook.com>
2023-10-19 15:33:03 +0000

    Add a function to free a stack

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * free_stack - Frees a doubly linked list representing a stack
+ * @head: Pointer to head of stack
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *current;
+
+	current = head;
+	while (head)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
+}
+
